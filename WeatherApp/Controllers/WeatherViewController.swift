d5a8b82453ab05560bf03bee755e465ca2400357
thejorabek <boyxorozovjorabek@gmail.com>
2024-12-09 13:35:55 +0500

    first

---
+++ WeatherApp/Controllers/WeatherViewController.swift
@@ -0,0 +1,19 @@
+ //
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Jorabek Boyxorozov on 09/12/24.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
