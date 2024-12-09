5f03bf3f7b321b8158b2f7d7d3295760041f606a
thejorabek <boyxorozovjorabek@gmail.com>
2024-12-09 19:12:23 +0500

    second

---
+++ WeatherApp/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  WeatherApp
+//
+//  Created by Jorabek Boyxorozov on 09/12/24.
+//
+
+import UIKit
+
+class SettingsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
